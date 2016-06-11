2a15d2bc47f427f6eecd44491c0c9845a0453261
lukehedger <luke.hedger@gmail.com>
2016-06-11 14:59:45 +0100

    color tweaks, time styles

--- app/js/dialogue/components/time.js
+++ app/js/dialogue/components/time.js
@@ -1,11 +1,17 @@
 import React from 'react'
-// import styles from 'css/components/time.css'
+import moment from 'moment'
+
+import styles from 'css/components/dialogue/time.css'
 
 const Component = ({ time }) => {
 
+  const hour = moment(time).format('h')
+  const minutes = moment(time).format('mm')
+  const period = moment(time).format('A')
+
   return (
-    <div>
-      {time}
+    <div className={styles.base}>
+      {hour}:{minutes} {period}
     </div>
   )
 
